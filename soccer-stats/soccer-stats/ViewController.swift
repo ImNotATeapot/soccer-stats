979dc9938b5fad0305221c98936be8c70e661f50
Pearl <imnotateapot@gmail.com>
2017-11-09 21:29:01 -0500

    Created project

---
+++ soccer-stats/soccer-stats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soccer-stats
+//
+//  Created by Pearl on 11/9/2560 BE.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ soccer-stats/soccer-statsTests/soccer_statsTests.swift
@@ -0,0 +1,36 @@
+//
+//  soccer_statsTests.swift
+//  soccer-statsTests
+//
+//  Created by Pearl on 11/9/2560 BE.
+//
+//
+
+import XCTest
+@testable import soccer_stats
+
+class soccer_statsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
